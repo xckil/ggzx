2e592ee0012158b85850096b122f500b35e782a6
xckil <1404787452@qq.com>
2023-08-15 23:02:25 +0800

    完善todo页面，还需处理路由跳转

--- src/view/todo/tolist.vue
+++ src/view/todo/tolist.vue
@@ -3,16 +3,37 @@
     <!--添加事项-->
     <header class="header">
       <h1>todos</h1>
-      <input @keyup.enter="handlekeydown" v-model="tg" class="new-todo" placeholder="接下来该做什么" autofocus type="text" />
+      <input
+        @keyup.enter="handlekeydown"
+        v-model="tg"
+        class="new-todo"
+        placeholder="接下来该做什么"
+        autofocus
+        type="text"
+      />
     </header>
     <!-- 代办事项 -->
     <section class="main">
-      <input id="toggle-all" class="toggle-all" type="checkbox" @click="setalltask" />
+      <input
+        id="toggle-all"
+        class="toggle-all"
+        type="checkbox"
+        @click="setalltask"
+      />
       <label for="toggle-all">Mark all as complete</label>
       <ul class="todo-list">
-        <li v-for="item in filteroption"  :key="item.id" :class="{ completed: item.completed }">
+        <li
+          v-for="item in filteroption"
+          :key="item.id"
+          :class="{ completed: item.completed }"
+        >
           <div class="view">
-            <input type="checkbox" class="toggle" @change="completedtask(item)" :checked="item.completed" />
+            <input
+              type="checkbox"
+              class="toggle"
+              @change="completedtask(item)"
+              :checked="item.completed"
+            />
             <label>{{ item.content }}</label>
             <button class="destroy" @click="deletetask"></button>
           </div>
@@ -65,18 +86,16 @@ const tg = ref('')
 
 const visiably = ref('all')
 
-const count = computed(() => todos.value.filter(item => !item.completed).length)
+const count = computed(
+  () => todos.value.filter((item) => !item.completed).length,
+)
 
 const filteroption = computed(() => {
-
-  if (visiably.value == 'all')
-    return todos.value
+  if (visiably.value == 'all') return todos.value
   else if (visiably.value == 'active')
-    return todos.value.filter(item => !item.completed)
-  else
-    return todos.value.filter(item => item.completed)
+    return todos.value.filter((item) => !item.completed)
+  else return todos.value.filter((item) => item.completed)
 })
-
 
 function OnHashChange() {
   const hash = window.location.hash.replace('/#\/?', '')
@@ -88,25 +107,22 @@ function OnHashChange() {
   }
 }
 
-onMounted(() =>{
+onMounted(() => {
   console.log('onmounted正在执行')
   OnHashChange()
 })
 
 //监听键盘事件
 function handlekeydown() {
-
-  if (tg.value == "")
-    return;
+  if (tg.value == '') return
 
   let kk = {
-    id: (Math.random() * 81) + 20,
+    id: Math.random() * 81 + 20,
     content: tg.value,
     completed: false,
   }
   todos.value.push(kk)
-  tg.value = ""
-
+  tg.value = ''
 }
 
 // 完成任务
@@ -118,16 +134,14 @@ function completedtask(item: any) {
 function deletetask(event: any) {
   console.log(event.target)
   todos.value.splice(event.target.key, 1)
-
 }
 
 // 设置所有的任务
 function setalltask(event: any) {
-  todos.value.forEach(element => {
+  todos.value.forEach((element) => {
     element.completed = event.target.checked
   })
 }
-
 </script>
 
 <style src="@/styles/tolist.css"></style>
